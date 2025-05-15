2c5d718dd1ccddbf11cbc8d0e6e5553ee130e1f3
TUF <TUF@LAPTOP-AE0MF6OR>
2025-05-15 15:11:13 +0330

    add entity and dbcontext

---
+++ Wallets/Shared/Persistance/Entities/Wallet.cs
@@ -0,0 +1,38 @@
+﻿namespace Wallets.Shared.Persistance.Entities;
+
+public class Wallet
+{
+    public int Id { get; set; }
+    public Guid ProfileId { get; set; }
+    public string Title { get; set; }
+    public ICollection<Currency> Currencies { get; set; }
+
+    public Wallet(Guid profileId, string title, Currency[] currencies)
+    {
+        ProfileId = profileId;
+        Title = title;
+        Currencies = currencies;
+    }
+}
+
+
+public class WalletConfiguration : IEntityTypeConfiguration<Wallet>
+{
+    public void Configure(EntityTypeBuilder<Wallet> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.ProfileId)
+               .IsRequired();
+
+        builder.Property(x => x.Title)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        //builder.HasMany<>(x => x.Raito)
+        //    .HasPrecision(18, 4)
+        //    .IsRequired();
+
+    }
+
+}
--- Wallets/Shared/Persistance/WalletDbContext.cs
+++ Wallets/Shared/Persistance/WalletDbContext.cs
@@ -4,6 +4,7 @@ namespace Wallets.Shared.Persistance;
 public class WalletDbContext(DbContextOptions<WalletDbContext> dbContextOptions) : DbContext(dbContextOptions)
 {
     public DbSet<Currency> currencies { get; set; }
+    public DbSet<Wallet> wallets { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
